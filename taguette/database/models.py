25341d479491313a9431d26a93d9e2f3f1fd72bb
JackSalazar <jsalazarlongsnap@icloud.com>
2026-04-28 18:56:07 -0700

    Added table tags_directorys, with the parent table projects and child table tags

--- taguette/database/models.py
+++ taguette/database/models.py
@@ -223,6 +223,9 @@ class Project(Base):
     tags = relationship('Tag', cascade='all,delete-orphan', order_by='Tag.id',
                         passive_deletes=True,
                         back_populates='project')
+    
+    tags_directorys = relationship("TagsDirectory", back_populates='project')
+
 
     def __repr__(self):
         return '<%s.%s %r %r>' % (
@@ -635,6 +638,10 @@ class Tag(Base):
         back_populates='tags',
     )
 
+    tags_directory_id = Column(Integer, ForeignKey('tags_directorys.id', ondelete='CASCADE'), nullable=True, index=True)
+    tags_directorys = relationship('TagsDirectory', back_populates='tags')
+
+
     def __repr__(self):
         return '<%s.%s %r %r project_id=%r>' % (
             self.__class__.__module__,
@@ -679,3 +686,25 @@ Tag.documents_count = column_property(
     .correlate_except(Highlight.__table__, highlight_tags)
     .scalar_subquery()
 )
+
+class TagsDirectory(Base):
+    
+    __tablename__ = 'tags_directorys' #Name of the table itself
+    __table_args__ = ({'sqlite_autoincrement': True},) #Exists because it's there for all other classes
+
+    id = Column(Integer, primary_key=True) #column name = type. primary_key is saying that this is the main identifier that can be used in indexing
+    project_id = Column(Integer, ForeignKey('projects.id', ondelete='CASCADE'), index=True) #ForeignKey is labeling the parent table and what column should be used as the identifier.
+    name = Column(String(200), nullable=False) #Name of the directory itself
+
+    tags = relationship('Tag', back_populates='tags_directorys' )
+    project = relationship('Project', back_populates='tags_directorys')
+    
+    def __repr__(self):
+        return '<%s.%s %r %r project_id=%r>' % (
+            self.__class__.__module__,
+            self.__class__.__name__,
+            self.id,
+            self.name,
+            self.project_id,
+        )
+
